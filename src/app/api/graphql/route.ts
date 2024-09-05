d9c00017893b78bd27d8864650c971883575f9de
Filip Masny <filip.masny@gmail.com>
2024-09-06 01:35:27 +0200

    add prisma graphql to the code

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -4,70 +4,76 @@ import { NextRequest } from 'next/server';
 import { ApolloServer, BaseContext } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 
+import prisma from '@/prisma/prisma';
+
 const resolvers: BaseContext = {
-  Query: {
-    hello: () => 'world',
-    program: () => 'program',
-    vekovaKat: () => 'vekovaKat',
-    expertskeOdborky: () => 'expertskeOdborky',
-    programKat: () => 'programKat',
-    stupen: () => 'stupen',
-  },
+	Query: {
+		vekovaKat: async () => {
+			const vekovaKats = await prisma.vekova_kat.findMany({
+				orderBy: {
+					vekova_kat_id: 'asc',
+				},
+			});
+			return vekovaKats.map((vekovaKat) => ({
+				id: vekovaKat.vekova_kat_id,
+				name: vekovaKat.vekova_kat_name,
+			}));
+		},
+	},
 };
 
 const typeDefs = gql`
-  type Uloha {
-    uloha_id: Int!
-    program_id: Int!
-    cislo_ulohy: Int!
-    text_ulohy: String!
-    potrebny_pocet_poduloh: Int
-    podulohy: JSON
-  }
+	scalar JSON
 
-  scalar JSON
+	type Uloha {
+		uloha_id: Int!
+		program_id: Int!
+		cislo_ulohy: Int!
+		text_ulohy: String!
+		potrebny_pocet_poduloh: Int
+		podulohy: JSON
+	}
 
-  type ProgramKat {
-    id: Int!
-    name: String!
-  }
+	type ProgramKat {
+		id: Int!
+		name: String!
+	}
 
-  type VekKat {
-    id: Int!
-    name: String
-  }
+	type VekKat {
+		id: Int!
+		name: String
+	}
 
-  type Stupen {
-    id: Int!
-    name: String!
-  }
+	type Stupen {
+		id: Int!
+		name: String!
+	}
 
-  type ExpertskeOdborky {
-    id: Int!
-    name: String!
-    foto: String!
-  }
+	type ExpertskeOdborky {
+		odborky_id: Int!
+		odborky_name: String!
+		odborky_foto: String!
+	}
 
-  type Program {
-    id: Int!
-    vekova_kat: VekKat!
-    program_kat: ProgramKat!
-    name: String!
-    photo: String!
-    ulohy: [Uloha!]!
-    stupen: Stupen
-    expertske_odborky: ExpertskeOdborky
-    info: JSON
-  }
+	type Program {
+		program_id: Int!
+		vekova_kat: VekKat!
+		program_kat: ProgramKat!
+		program_name: String!
+		program_photo: String!
+		ulohy: [Uloha!]!
+		stupen: Stupen
+		expertske_odborky: ExpertskeOdborky
+		program_info: JSON
+	}
 
-  type Query {
-    hello: String
-    program(program_id: Int!, vekova_kat_id: Int!): [Program!]!
-    vekovaKat: [VekKat!]!
-    expertskeOdborky: [ExpertskeOdborky!]!
-    programKat: [ProgramKat!]!
-    stupen: [Stupen!]!
-  }
+	type Query {
+		program(program_id: Int!, vekova_kat_id: Int!): [Program!]!
+		vekovaKat: [VekKat!]!
+		expertskeOdborky: [ExpertskeOdborky!]!
+		programKat: [ProgramKat!]!
+		stupen: [Stupen!]!
+	}
 `;
 
 const server = new ApolloServer({
